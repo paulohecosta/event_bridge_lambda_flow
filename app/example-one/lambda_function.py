b00cc008c675785c05c73ed05a2c9296327df6c0
Paulo Costa <paulohecosta@gmail.com>
2021-12-15 22:05:02 -0300

    Fixes

--- app/example-one/lambda_function.py
+++ app/example-one/lambda_function.py
@@ -4,7 +4,6 @@ import os
 
 def lambda_handler(event, context):
     print('lambda_handler example step one')
-    
     client = boto3.client('sns')
 
     response = client.publish(
@@ -16,6 +15,24 @@ def lambda_handler(event, context):
         TargetArn=os.getenv('SNS_NAME'),
         Message=json.dumps({"foo": "message out filter"}),
         MessageAttributes={'period': {'DataType': 'String', 'StringValue': 'daily'}}
+    )
+
+    client = boto3.client('events')
+    response = client.put_events(
+        Entries=[ 
+            { 
+                'Source': 'custom.example', 
+                'DetailType': 'transaction', 
+                'Detail': '{ "period": "weekly" }', 
+                'EventBusName': 'default' 
+            },
+            { 
+                'Source': 'custom.example', 
+                'DetailType': 'transaction', 
+                'Detail': '{ "period": "daily" }', 
+                'EventBusName': 'default' 
+            },
+        ]
     )
 
     return {
--- app/example-two/lambda_function.py
+++ app/example-two/lambda_function.py
@@ -2,7 +2,6 @@ import json
 
 def lambda_handler(event, context):
     print(event)
-    print(context)
     print('lambda_handler example step two')
     return {
         'statusCode': 200,
